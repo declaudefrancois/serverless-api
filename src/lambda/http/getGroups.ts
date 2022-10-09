4aa78ae40b2e355a47b35a13a836e4a38e7ba8cb
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-10-09 16:03:05 +0100

    initial commit (GET /groups endpoint)

---
+++ src/lambda/http/getGroups.ts
@@ -0,0 +1,27 @@
+import 'source-map-support/register';
+import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import * as AWS from "aws-sdk";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const groupsTable = process.env.GROUPS_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log("Processing event ", event);
+
+  const result = await docClient.scan({
+    TableName: groupsTable,
+  }).promise();
+
+  const items = result.Items;
+
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items,
+    }, null, 2)
+  }
+}
