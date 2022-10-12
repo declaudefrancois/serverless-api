fc268cbb05fdbf8fea54cdf26247f69bb77180c0
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-10-12 21:44:33 +0100

    feat: setup a lambda function for s3:ObjectCreated events

---
+++ src/lambda/s3/sendNotifications.ts
@@ -0,0 +1,9 @@
+import "source-map-support/register";
+import { S3Handler, S3Event } from 'aws-lambda';
+
+export const handler: S3Handler = async (event: S3Event) => {
+  for (const record of event.Records) {
+    const key = record.s3.object.key;
+    console.log('Processing S3 item with key :', key);
+  }
+}
